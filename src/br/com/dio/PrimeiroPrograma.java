eff4030cf688179676660499c1cb4e13ebce1b38
pbmariano <pablo.almeida.mariano@gmail.com>
2021-12-21 23:15:41 -0300

    criando clss Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -26,3 +26,37 @@ public class PrimeiroPrograma {
         System.out.println("Hello Word " + (a+b));*/
     }
 }
+ class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+
+     public Livro() {}
+
+     public String getNome() {
+         return nome;
+     }
+
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+
+     public Integer getNumPaginas() {
+         return numPaginas;
+     }
+
+     public void setNumPaginas(Integer numPaginas) {
+         this.numPaginas = numPaginas;
+     }
+
+     @Override
+     public String toString() {
+         return "Livro{" +
+                 "nome='" + nome + '\'' +
+                 ", numPaginas=" + numPaginas +
+                 '}';
+     }
+ }
+
+
+
